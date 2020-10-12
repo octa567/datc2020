d7e0dc1fb18c7eaed15993b2d84e1d9822f70fdd
Damian Octavian-Gabriel <62719951+octa567@users.noreply.github.com>
2020-10-12 09:48:27 +0300

    Add files via upload

---
+++ L03/New/New/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace New
+{
+    class Program
+    {
+        static string[] Scopes = { DriveService.Scope.Drive};
+        static string ApplicationName = "Drive API .NET Quickstart";
+        static void Main(string[] args)
+        {
+            Initializare();
+        }
+        static void Initializare()
+        {
+             UserCredential credential;
+
+            using (var stream =
+                new FileStream("client_secret_29156076459.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    Environment.UserName,
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Drive API service.
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+    }
+}
+ 
